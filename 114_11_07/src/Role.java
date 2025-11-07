74e17beb8dee12ac2a29ba91b351f403dc658193
a110223039 <a110223039@mail.shu.edu.tw>
2025-11-07 15:02:55 +0800

    Add files via upload

---
+++ 114_11_07/src/Role.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import javax.swing.plaf.PanelUI;
+
+public class Role extends Object {
+    private String name;
+
+    private int health;
+
+    private int attackPower;
+
+    public Role(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    public  String getName() {
+        return name;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isAlice() {
+        return this.health > 0;
+    }
+
+    @Override
+    public String toString() {
+        return   name + "生命值: " + health;
+    }
+}
